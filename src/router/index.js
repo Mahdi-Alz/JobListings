c4fe1f66014e7655c93aa3c65789fe69e11f4562
Mahdi Alizadeh <mahdialz.dev@gmail.com>
2026-06-06 15:25:01 +0330

    feat: add notFound page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+    {
+      // In a web router, the colon (:) is a special flag that means: "This part of the URL is a wild card variable, not a literal word."
+      path: "/:catchAll(.*)",
+      name: "not found",
+      component: NotFoundView,
     },
   ],
 });
