09049bfb247fd7b1f1d57da890d313b8ab2887ba
Mahdi Alizadeh <mahdialz.dev@gmail.com>
2026-06-06 15:47:39 +0330

    feat: hard-code JobView

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import JobView from "@/views/JobView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+    {
+      path: "/jobs/:id",
+      name: "job",
+      component: JobView,
     },
     {
       // In a web router, the colon (:) is a special flag that means: "This part of the URL is a wild card variable, not a literal word."
