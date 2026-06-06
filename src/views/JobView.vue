5868c22a2ca4377793b27c69b4cffb667ecc1ea9
Mahdi Alizadeh <mahdialz.dev@gmail.com>
2026-06-06 23:23:38 +0330

    feat: add backButton for job listing

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { PulseLoader } from "vue-spinner";
 import axios from "axios";
+import BackButton from "@/components/BackButton.vue";
 import { reactive, onMounted } from "vue";
 import { useRoute, RouterLink } from "vue-router";
 
@@ -24,6 +25,7 @@ onMounted(async () => {
 </script>
 
 <template>
+  <BackButton />
   <section v-if="!state.isLoading" class="bg-green-50">
     <div class="container m-auto py-10 px-6">
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -88,6 +90,9 @@ onMounted(async () => {
           <!-- Manage -->
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Job</h3>
+            <!-- Plain Quotes ("text" or 'text'): Use these when your string is basic text and doesn't change.
+            Backticks (`text ${variable}`): Use these when you need to mix text with JavaScript variables.
+            Vue Attributes (:attribute="js_code"): The outer quotes are for HTML; the inner backticks are for JavaScript. -->
             <RouterLink
               :to="`/jobs/edit/${state.job.id}`"
               class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
