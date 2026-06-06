42d434dfc8d8920196b03e6bf3b626e3b6e99e22
Mahdi Alizadeh <mahdialz.dev@gmail.com>
2026-06-06 22:32:27 +0330

    fix: resolve calling data from json server

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,6 +1,8 @@
 <script setup>
+import { RouterLink } from "vue-router";
 import JobListing from "./JobListing.vue";
 import { reactive, defineProps, onMounted } from "vue";
+import { PulseLoader } from "vue-spinner";
 import axios from "axios";
 defineProps({
   limit: Number,
@@ -9,14 +11,19 @@ defineProps({
     default: false,
   },
 });
-const jobs = reactive([]);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
 
 onMounted(async () => {
   try {
     const response = await axios.get("http://localhost:8000/jobs");
-    jobs.value = response.data;
+    state.jobs = response.data;
   } catch (error) {
     console.log("Error fetching jobs");
+  } finally {
+    state.isLoading = false;
   }
 });
 </script>
@@ -27,9 +34,12 @@ onMounted(async () => {
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(0, limit || jobs.length)"
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
           :job="job"
         />
